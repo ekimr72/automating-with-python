a44d5b0342060b1a35cae01442a3c18adfa1f5ae
Erkki Ruskio <erkki.ruskio@cybercom.com>
2019-10-27 20:10:53 +0200

    Add notifier serverless project

---
+++ 0202-notifon/scale-up.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+import boto3
+
+session = boto3.Session(profile_name='default')
+
+as_client = session.client('autoscaling')
+
+as_client.execute_policy(AutoScalingGroupName='Notifon', PolicyName='Scale up')
